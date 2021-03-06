b51fcd5cd4556e1641f390af2053a015ea095841
TominagaRyuto <esouunnpou@gmail.com>
2021-03-06 03:55:31 +0000

    [Add]users/edit機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable#, :validatable
   attachment :image
+  validates :name, :name_kana, :nickname, :postal_code, :address, :telephone_number, :encrypted_password, :email, presence: true
 end
