e4c16d24d6ad660a6cbc7db5b460106ccaac3fc1
TominagaRyuto <esouunnpou@gmail.com>
2021-03-06 09:11:25 +0000

    [Add]Addressバリデーション

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,6 @@
 class Address < ApplicationRecord
+  validates :name, :address, presence: true
+  validates :postal_code, length: { is: 7 }, numericality: { only_integer: true }
+
   belongs_to :user
 end
