b339170fb872a637a419c50b2480244336a1c615
TominagaRyuto <esouunnpou@gmail.com>
2021-03-06 10:07:52 +0000

    [Add]address/投稿、一覧表示機能

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,8 +1,18 @@
 class Public::AddressesController < ApplicationController
   def index
+    @address = Address.new
+    @addresses = current_user.addresses.all
   end
 
   def create
+    @address = Address.new(address_params)
+    @address.user_id = current_user.id
+    if @address.save
+      redirect_to addresses_path
+    else
+      @addresses = current_user.addresses.all
+      render :index
+    end
   end
 
   def update
@@ -12,5 +22,11 @@ class Public::AddressesController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:name, :postal_code, :address)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,11 @@
 Rails.application.routes.draw do
-  namespace :public do
-    get 'addresses/index'
-    get 'addresses/create'
-    get 'addresses/update'
-    get 'addresses/destroy'
-    get 'addresses/edit'
-  end
   scope module: :public do
     root to: 'homes#top'
       get 'about' => 'homes#about'
     resource :users, only:[:show, :edit, :update]
       get 'users/confirm' => 'users#confirm', as: 'users_confirm'
       patch 'users/cancellation' => 'users#cancellation', as: 'users_cancellation'
-    
+    resources :addresses, :except =>[:show, :new]
   end
   devise_for :admins
   devise_for :users
